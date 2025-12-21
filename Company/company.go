aa96daa7d118468282bf61869b98568fec5dbd33
ballm <alisherzhanibek@mail.ru>
2025-12-22 01:48:52 +0500

    structure

--- Company/company.go
+++ Company/company.go
@@ -1,1 +1,16 @@
 package Company
+
+type Employee interface {
+	GetDetails() string
+}
+type FullTimeEmployee struct {
+}
+type PartTimeEmployee struct {
+}
+type Company struct {
+	Employee map[string]Employee
+}
+
+func CompanyCLI() {
+
+}

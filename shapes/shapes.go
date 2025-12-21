4716cf085f7df804305b072ecebb6b7ed0a89403
ballm <alisherzhanibek@mail.ru>
2025-12-22 01:27:25 +0500

    all shapes with its area and perimeter are done

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -16,20 +16,67 @@ type Circle struct {
 	Radius float64
 }
 type Square struct {
+	Side float64
 }
 type Triangle struct {
+	Side1 float64
+	Side2 float64
+	Side3 float64
 }
 
+// func for retangle
+func (rectangle Rectangle) Area() float64 {
+	return rectangle.Length * rectangle.Width
+}
+func (rectangle Rectangle) Perimeter() float64 {
+	return 2 * (rectangle.Length + rectangle.Width)
+}
+
+// func for circle
 func (circle Circle) Area() float64 {
 	return math.Pi * math.Pow(circle.Radius, 2)
 }
 func (circle Circle) Perimeter() float64 {
 	return 2 * math.Pi * circle.Radius
 }
+
+// func for square
+func (square Square) Area() float64 {
+	return math.Pow(square.Side, 2)
+}
+func (square Square) Perimeter() float64 {
+	return 4 * square.Side
+}
+
+// func for triangle
+func (triangle Triangle) Area() float64 {
+	// bruh
+	var halfP = triangle.Perimeter() / 2
+	return math.Sqrt(halfP *
+		(halfP - triangle.Side1) *
+		(halfP - triangle.Side2) *
+		(halfP - triangle.Side3))
+}
+func (triagnle Triangle) Perimeter() float64 {
+	return triagnle.Side1 + triagnle.Side2 + triagnle.Side3
+}
+
 func ShapesCLI() {
 	var s Shape
-	s = Circle{Radius: 4}
-	fmt.Println("Circle Area", s.Area())
-	fmt.Println("C Perimeter:", s.Perimeter())
 
+	s = Rectangle{Length: 5, Width: 3}
+	fmt.Println("Rectangle area: ", s.Area())
+	fmt.Println("Rectangle Perimeter:", s.Perimeter())
+
+	s = Circle{Radius: 4}
+	fmt.Println("Circle area", s.Area())
+	fmt.Println("Circle perimeter:", s.Perimeter())
+
+	s = Square{Side: 4}
+	fmt.Println("Square area", s.Area())
+	fmt.Println("Square perimeter:", s.Perimeter())
+
+	s = Triangle{Side1: 3, Side2: 4, Side3: 5}
+	fmt.Println("Triangle area", s.Area())
+	fmt.Println("Triangle perimeter:", s.Perimeter())
 }

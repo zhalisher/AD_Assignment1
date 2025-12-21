9b8a2ab071425071cf9a94673815c061908adeaf
ballm <alisherzhanibek@mail.ru>
2025-12-21 23:55:19 +0500

    structure

--- Shapes/shapes.go
+++ Shapes/shapes.go
@@ -1,1 +1,19 @@
 package Shapes
+
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+type Rectangle struct {
+}
+type Circle struct {
+	radius float64
+}
+type Square struct {
+}
+type Triangle struct {
+}
+
+func ShapesCLI() {
+
+}

88838f306283c759f8305052ef6beb6a8b437ce8
ballm <alisherzhanibek@mail.ru>
2025-12-22 02:08:34 +0500

    structrure2.0(still in progress)

--- Company/company.go
+++ Company/company.go
@@ -1,5 +1,9 @@
 package Company
 
+import (
+	"fmt"
+)
+
 type Employee interface {
 	GetDetails() string
 }
@@ -8,9 +12,42 @@ type FullTimeEmployee struct {
 type PartTimeEmployee struct {
 }
 type Company struct {
-	Employee map[string]Employee
+	Employee map[uint64]Employee
+}
+
+// func for full time
+func (fullTime FullTimeEmployee) GetDetails() string {
+	return "Full Time Employee"
+}
+
+// func fot part time
+func (partTime PartTimeEmployee) GetDetails() string {
+	return "Part Time Employee"
 }
 
 func CompanyCLI() {
+	fmt.Println("Welcome to Employee Management System application!")
 
+	for loop := true; loop; {
+
+		var choice uint8
+
+		fmt.Println("==============================")
+		fmt.Println("Add Employee: 1")
+		fmt.Println("List Employee: 2")
+		fmt.Println("Exit: 3")
+		fmt.Println("==============================")
+
+		fmt.Println("What to do now?")
+		fmt.Scan(&choice)
+
+		switch choice {
+		case 1:
+		case 2:
+		case 3:
+			fmt.Println("Exiting from application")
+			loop = false
+		}
+
+	}
 }

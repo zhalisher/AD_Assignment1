61b2ebe4a1037656f278074e34e1b758ea3dfcb9
ballm <alisherzhanibek@mail.ru>
2025-12-22 10:52:37 +0500

    works

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "AD_Assignment1/Library"
+import (
+	"AD_Assignment1/company"
+)
 
+// to init gomod = go mod init packageName
+
+// %s → string
+// %d → integer
+// %f → float
+// %t → boolean
+// %v → "default" format (anything)
 func main() {
-	Library.Library()
+	//library.LibraryCLI()
+	//shapes.ShapesCLI()
+	company.CompanyCLI()
+	//bank.BankCLI()
 }

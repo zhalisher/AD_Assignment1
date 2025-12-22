c1942217fcb8817e2d87e8e0da7ce9458eb7b20f
ballm <alisherzhanibek@mail.ru>
2025-12-22 10:26:04 +0500

    added borrow + return also added a case where all borrowed book is listsed

--- library/library.go
+++ library/library.go
@@ -19,11 +19,15 @@ func (library *Library) AddBook(book Book) {
 	library.Books[book.Title] = book
 	fmt.Println("Book added: ", book.Title)
 }
-func (library *Library) BorrowBook(id int) {
-
+func (library *Library) BorrowBook(title string) {
+	book := library.Books[title]
+	book.IsBorrowed = true
+	library.Books[book.Title] = book
 }
-func (library *Library) ReturnBook(id int) {
-
+func (library *Library) ReturnBook(title string) {
+	book := library.Books[title]
+	book.IsBorrowed = false
+	library.Books[title] = book
 }
 
 func (library Library) ListAvailableBooks() {
@@ -47,7 +51,8 @@ func LibraryCLI() {
 		fmt.Println("Borrow book: 2")
 		fmt.Println("ReturnBook: 3")
 		fmt.Println("List all available book: 4")
-		fmt.Println("Exit: 5")
+		fmt.Println("List all borrowed book: 5")
+		fmt.Println("Exit: 6")
 		fmt.Println("==============================")
 
 		fmt.Println("What to do now?")
@@ -67,10 +72,49 @@ func LibraryCLI() {
 
 			library.AddBook(Book{ID: id, Title: title, Author: author})
 		case 2:
+			var title string
+
+			fmt.Println("Enter book title to borrow")
+			fmt.Scan(&title)
+			book, ok := library.Books[title]
+			if !ok {
+				fmt.Println("Book not found")
+			} else if book.IsBorrowed {
+				fmt.Println("Book is already borrowed")
+			} else {
+				library.BorrowBook(title)
+				fmt.Println("You borrowed:", title)
+			}
 		case 3:
+			var title string
+
+			fmt.Println("Enter book title to return")
+			fmt.Scan(&title)
+			book, ok := library.Books[title]
+			if !ok {
+				fmt.Println("Book not found")
+			} else if !book.IsBorrowed {
+				fmt.Println("Book was not borrowed")
+			} else {
+				library.ReturnBook(title)
+				fmt.Println("You returned:", title)
+			}
 		case 4:
 			library.ListAvailableBooks()
 		case 5:
+			fmt.Println("Borrowed books: ")
+			hasBorrowed := false
+			for _, b := range library.Books {
+				if b.IsBorrowed {
+					fmt.Printf("ID: %d, Title: %s, Author: %s\n", b.ID, b.Title, b.Author)
+					hasBorrowed = true
+				}
+			}
+			if !hasBorrowed {
+				fmt.Println("No borrowed books")
+			}
+
+		case 6:
 			fmt.Println("Exiting from application")
 			loop = false
 		}
